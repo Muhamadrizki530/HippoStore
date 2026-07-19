aaf11d217869459ae35fabf9f930a4a3082fbcd7
unknown <rizkymaulana3025@gmail.com>
2026-07-19 16:25:27 +0700

    Pesan commit

--- resources/views/partials/buttons.blade.php
+++ resources/views/partials/buttons.blade.php
@@ -1,12 +1,12 @@
 <div class="menu-container">
 
-    <a href="https://wa.me/6285654614766?text=Mau%20joki%20gendong%20dong" class="menu-card" target="_blank">
+    <a href="https://wa.me/6285876065715?text=Mau%20joki%20gendong%20dong" class="menu-card" target="_blank">
         <i class="bi bi-controller menu-icon"></i>
         <span>ORDER JOKGEN</span>
         <i class="bi bi-chevron-right arrow"></i>
     </a>
 
-    <a href="https://wa.me/6285654614766?text=Mau%20joki%20akun%20 dong" class="menu-card" target="_blank">
+    <a href="https://wa.me/6285876065715?text=Mau%20joki%20akun%20 dong" class="menu-card" target="_blank">
         <i class="bi bi-lightning-charge menu-icon"></i>
         <span>ORDER JOKI</span>
         <i class="bi bi-chevron-right arrow"></i>
@@ -18,8 +18,8 @@
         <i class="bi bi-chevron-right arrow"></i>
     </a>
 
-    <a href="https://x.com/Heyhippo1" class="menu-card" target="_blank">
-        <i class="bi bi-chat-left-text menu-icon"></i>
+    <a href="https://t.me/+xZ13zBCPRlUyMjg1" class="menu-card" target="_blank">
+        <i class="bi bi-telegram menu-icon"></i>
         <span>TESTIMONIALS</span>
         <i class="bi bi-chevron-right arrow"></i>
     </a>
